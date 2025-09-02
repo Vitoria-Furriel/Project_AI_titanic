4acdae31aa7dc62ed0b32dec7e5d9bbcef763bb0
Vitoria Furriel <kimseoahseo@gmail.com>
2025-09-02 20:51:04 -0300

    Atualizado pré-processamento, add StandardScaler

--- project_AI.py
+++ project_AI.py
@@ -1,19 +1,21 @@
+# -*- coding: utf-8 -*-
+
 # Importing the necessary libraries
 import pandas as pd
 import numpy as np
 
-dataset = pd.read_csv("Titanic.csv", sep=";" , encoding="utf-8-sig") #read the dataset
-
-dataset = dataset.drop(["Name", "Ticket", "Cabin" , "PassengerId"], axis=1)
-dataset.head()
+dataset = pd.read_csv("Titanic_1.csv", delimiter= ";") #read the dataset
 
 X = dataset.drop("Survived", axis=1) #já cria o dataframe desejado, todos sem o survived TREINAMENTO
 y = dataset["Survived"] #Colocamos direto para não errarmos o idex/posição. TESTE
+X = dataset.drop(["Name", "Ticket", "Cabin" ], axis=1)
 X.head()
 
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.preprocessing import LabelEncoder
+
+print(dataset)
 
 categorial_features = ["Sex", "Embarked"] #Definimos quais são os features a serem transformados.
 
@@ -22,13 +24,21 @@ categorial_features = ["Sex", "Embarked"] #Definimos quais são os features a se
 ct = ColumnTransformer(transformers=[("encoder", OneHotEncoder(),categorial_features)],remainder="passthrough")
 
 X_transformed = ct.fit_transform(X)
-#temos que colocar X porque é nele que está Sex e Embarked para transformar.
 
-#Foram transformadas com sucesso.
-#As colunas transformadas, o OHE colocam no inicio da dataframe.
-#Enquanto remainder ="passthrough" são colocados depois.
 
-#Pelo fato de ter colocado dataset dentro do transoform
+print(X_transformed[:70])
 
-print(X_transformed[:5])
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
 
+X_train, X_test, y_train, y_test = train_test_split(X_transformed, y, test_size=0.2, random_state =42)
+
+cols = [5,8,11]
+
+
+
+sc = StandardScaler()
+X_train[:,cols] = sc.fit_transform(X_train[:,cols])
+X_test[:,cols] = sc.transform(X_test[:,cols])
+
+print(X_train)
